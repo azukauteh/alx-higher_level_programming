57bc401bbbeae1bd683a428590ebf1c9683cb1a1
azukauteh <aazure263@gmail.com>
2023-06-06 11:02:07 -0700

    a program that prints numbers from 0 to 99.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
+# Appends each letter and concatenate
+output = ""
 # prints numbers from 0 to 99.separated by ,, followed by a space
 for number in range(100):
     if number == 99:
-        print("{:02}".format(number) + "\n")
+        output += "{:02}".format(number)
     else:
-        print("{:02}".format(number), end=", ")
+        output += "{:02}, ".format(number)
+    print("{0:02}".format(number), end=", ")
