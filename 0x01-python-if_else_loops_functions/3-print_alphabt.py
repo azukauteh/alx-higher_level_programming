03f1a86d55d7fb540e1fa952f7593af93101c3e6
azukauteh <aazure263@gmail.com>
2023-06-06 08:17:32 -0700

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
-# Print all the alphabet  letters except q and e
+#appends each letter and concatenate
+output = ""
+"""Print all the alphabet  letters except q and e"""
 for letter in range(97, 123):
   if chr(letter) != 'q' and chr(letter) != 'e':
-       print("{}".format(chr(letter)))
+       output += chr(letter)
+       print("{}".format(chr(letter)), end="")
