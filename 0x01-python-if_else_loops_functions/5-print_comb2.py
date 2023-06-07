24a4a694747e8a807c5a7bd0d21a683fd412feab
azukauteh <aazure263@gmail.com>
2023-06-07 10:43:04 -0700

    function that prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,10 +2,10 @@
 
 # Appends each letter and concatenate
 output = ""
-# Prints numbers from 0 to 99, separated by ", ", with the last number followed by a new line
+# Prints numbers from 0 to 99, separated by ",followed by a new line
 for number in range(100):
     if number == 99:
         output += "{:02}".format(number)
     else:
         output += "{:02}".format(number)
-    print( "{:02},".format(number), end="")
+    print("{:02},".format(number), end="")
