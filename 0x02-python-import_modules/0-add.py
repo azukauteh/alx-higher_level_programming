e5989fc63a76b7ceee4e5218ce7a37723aa3a8e0
azukauteh <aazure263@gmail.com>
2023-06-08 06:44:13 -0700

    a program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-
 if __name__ == "__main__":
+
+
     # calculate the sum of 1 and 2."
     a = 1
     b = 2
-    sum = add(a, b)
+    sum =  add(a, b)
 
     # print function with string format to display integers
     print("{} + {} = {}".format(a, b, sum) + "\n")
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+
+    # int args:
+    a = 10
+    b = 5
+# Print the sum, difference, multiple and quotient of 10 and 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)) + "\n")
