dfdbba9be055e2416c2d6b59c5284fad2b8cac72
azukauteh <aazure263@gmail.com>
2023-06-08 06:14:46 -0700

    a program that imports the function def add(a, b):

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from add_0 import add
 
-# My addition function
-def add(a, b):
-    # Args:
+if __name__ == "__main__":
+    # calculate the sum of 1 and 2."
     a = 1
     b = 2
+    sum = add(a, b)
 
-    #  Returns:
-    return (a+b)
+    # print function with string format to display integers
+    print("{} + {} = {}".format(a, b, sum) + "\n")
