6d979f202424d07e4f7d6fd5e650c3873f4ed126
azukauteh <aazure263@gmail.com>
2023-08-16 07:54:28 -0700

    script that inserts a new row in the table first_table (database hbtn_0c_0)

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,10 @@
-- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- New row:
+--     id = 89
+--     name = Best School
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
