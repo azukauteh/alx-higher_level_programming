85075b462a07736cd172ca8968f83a203dffd525
azukauteh <aazure263@gmail.com>
2023-08-17 10:06:07 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- Lists all privileges of the users user_0d_1.
--- lists all privileges of the users user_0d_2.
-
+-- Lists all privileges of the user user_0d_1.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lists all privileges of the user user_0d_2.
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
