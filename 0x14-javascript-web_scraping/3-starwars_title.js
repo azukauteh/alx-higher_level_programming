582a32411c8ed0c45325062c8d4a6a44ee5fe52c
azukauteh <aazure263@gmail.com>
2023-10-24 06:49:16 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Construct the URL for the Star Wars API using the film ID provided as a command line argument.
+// Construct the URL for the Star Wars API.
 const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-// Send an HTTP GET request to the specified URL and log the film title if successful.
+// Send an HTTP GET request to the specified UR.
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
