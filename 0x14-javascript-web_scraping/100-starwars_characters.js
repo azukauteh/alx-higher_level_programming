76071ffdc00139ee36aca73670ec8f7053f49db9
azukauteh <aazure263@gmail.com>
2023-10-25 01:27:04 -0700

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const filmId = process.argv[2];
-
 if (!filmId) {
   console.log('Usage: ./100-starwars_characters.js <film_id>');
   process.exit(1);
 }
-
 const url = 'https://swapi.dev/api/films/' + filmId;
-
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
