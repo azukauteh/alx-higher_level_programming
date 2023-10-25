ef89a6f75bd3964882a1ccbcc3147c0f485446c2
azukauteh <aazure263@gmail.com>
2023-10-25 01:33:28 -0700

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
-const filmId = process.argv[2];
-if (!filmId) {
-  console.log('Usage: ./100-starwars_characters.js <film_id>');
+const movieId = process.argv[2];
+if (!movieId) {
+  console.log('Usage: ./100-starwars_characters.js <movie_id>');
   process.exit(1);
 }
-const url = 'https://swapi.dev/api/films/' + filmId;
-request(url, function (error, response, body) {
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected response:', response.statusCode);
   } else {
-    try {
-      // Attempt to parse the response body as JSON
-      const data = JSON.parse(body);
-      console.log(data.title);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-    }
+    const film = JSON.parse(body);
+    console.log(film.title);
+    console.log('Characters:');
+
+    film.characters.forEach(characterUrl => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error:', charError);
+        } else if (charResponse.statusCode !== 200) {
+          console.error('Unexpected response:', charResponse.statusCode);
+        } else {
+          const character = JSON.parse(charBody);
+          console.log(character.name);
+        }
+      });
+    });
   }
 });
